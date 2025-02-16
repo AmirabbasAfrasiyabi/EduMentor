f49fde7419ae65a6aa55a0de3f2c628094e451ef
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2025-02-16 21:34:56 +0330

    CREATED first app

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,15 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 
-from mysite.views import http_test , json_test
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    #path( 'url add' , 'view')
-    path('http_test/', http_test),
-    path('json_test/', json_test),
-
-    
+    path('',include('website.urls'))    
 ]

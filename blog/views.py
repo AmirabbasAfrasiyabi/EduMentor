d9b20e6a21340311559c4a70f74aff5eed51c8ea
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2025-02-26 22:54:57 +0330

    solve all problem

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse , JsonResponse 
+from django.http import HttpResponse , JsonResponse
 
 def blog_view(request):
-    return render (request ,'blog/blog-home.html')
-    
+    return render(request, 'blog/blog-home.html')
+
 def blog_single(request):
-    context = {'title':'Post one' , 'content':'this is a post one for this blog' , 'author':'amirabbas'}
-    return render (request ,'blog/blog-single.html',context)
-    pass
+    # اضافه کردن تگ و تاریخ به context
+    context = {
+        'title': 'Post one',
+        'content': 'this is a post one for this blog',
+        'author': 'amirabbas',
+        'published_date': '1403/12/12',
+        'tags': ['marketing'],  # تگ‌ها
+    }
+    return render(request, 'blog/blog-single.html', context)

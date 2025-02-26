547df9db219924645132fe29dc4b1527785e116a
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2025-02-26 16:31:39 +0330

    created blog app

--- website/views.py
+++ website/views.py
@@ -9,3 +9,6 @@ def about_view(request):
 
 def contact_view(request):
     return render (request ,'website/contact.html')
+
+def course_view(request):
+    return render (request ,'website/course.html')

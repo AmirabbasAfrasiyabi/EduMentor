f6ed240227649f4ca897df56fd9394efb0703119
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2025-02-26 18:47:08 +0330

    update

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,6 @@ def blog_view(request):
     return render (request ,'blog/blog-home.html')
     
 def blog_single(request):
-    return render (request ,'blog/blog-single.html')
+    context = {'title':'Post one' , 'content':'this is a post one for this blog' , 'author':'amirabbas'}
+    return render (request ,'blog/blog-single.html',context)
     pass
